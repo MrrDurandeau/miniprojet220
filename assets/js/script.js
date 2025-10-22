98f1e7ffa54ab4e8dde075bec9530bc625ba02a2
Aymbonne <aymeric.bonne@etu.unilim.fr>
2025-10-22 19:24:22 +0200

    Uniquement pour le slider script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll('.slide');
+    var index = 0;
 
+    document.querySelector('.next').addEventListener('click', function(){
+        slides[index].classList.remove('active');
+        index++;
+        if (index >= slides.length) {
+            index = 0;
+        }
+        slides[index].classList.add('active')
+    })
+
+     document.querySelector('.prev').addEventListener('click', function(){
+        slides[index].classList.remove('active');
+        index--;
+        if (index == -1) {
+            index = 0;
+        }
+        slides[index].classList.add('active')
+    })
+})
