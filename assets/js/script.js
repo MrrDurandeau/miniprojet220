3b1ddfc86cba4bebf55c45d43f0d22140dc26d67
MorgenthalerXavier <xavier.morgenthaler@etu.unilim.fr>
2025-10-22 22:00:20 +0200

    Modification du js existant et ajout de la section séismes importants - script.js
    Le code pour les sliders n'affectent plus que la section concernée.
    
    Le code pour le slider des séismes importants, et pour l'API call des 10 séismes les plus importants est ajouté.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const slides = document.querySelectorAll('.slide');
+	// Partie séismes connus
+	const section_intro = document.getElementsByClassName('section_intro')[0];
+    const slides = section_intro.querySelectorAll('.slide');
     var index = 0;
 
-    document.querySelector('.next').addEventListener('click', function(){
+    section_intro.querySelector('.next').addEventListener('click', function(){
         slides[index].classList.remove('active');
         index++;
         if (index >= slides.length) {
             index = 0;
         }
-        slides[index].classList.add('active')
+        slides[index].classList.add('active');
     })
 
-     document.querySelector('.prev').addEventListener('click', function(){
+     section_intro.querySelector('.prev').addEventListener('click', function(){
         slides[index].classList.remove('active');
         index--;
         if (index == -1) {
             index = 0;
         }
-        slides[index].classList.add('active')
+        slides[index].classList.add('active');
     })
+	// Partie tremblements de terre à haute magnitude
+	const section_seismes_importants = document.getElementsByClassName('section_seismes_importants')[0];
+	const slides_sec2 = section_seismes_importants.querySelectorAll('.slide');
+	var index_sec2 = 0;
+	const date = new Date();
+	const endtime = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+	const starttime = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()-1}`;
+	const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${starttime}&endtime=${endtime}&minmagnitude=6&limit=10`;
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			let i = 0;
+			data.features.forEach(earthquake => {
+				fillQuakeSlide(slides_sec2,i,earthquake);
+				i++;
+			});
+		})
+	
+	section_seismes_importants.querySelector('.next').addEventListener('click', function(){
+        slides_sec2[index_sec2].classList.remove('active');
+        index_sec2++;
+        if (index_sec2 >= slides_sec2.length) {
+            index_sec2 = 0;
+        }
+        slides_sec2[index_sec2].classList.add('active');
+    })
+
+     section_seismes_importants.querySelector('.prev').addEventListener('click', function(){
+        slides_sec2[index_sec2].classList.remove('active');
+        index_sec2--;
+        if (index_sec2 == -1) {
+            index_sec2 = 0;
+        }
+        slides_sec2[index_sec2].classList.add('active');
+    })
+	
+	function fillQuakeSlide(slides,index,data) {
+		let quakeDateObject = new Date(data.properties.time);
+		let quakeDate = `${quakeDateObject.getDate()}-${quakeDateObject.getMonth()+1}-${quakeDateObject.getFullYear()}`;
+		slides[index].innerHTML=`<p>${data.properties.place}<br><span>Magnitude ${data.properties.mag}</span><br><span>${quakeDate}</span></p>`;
+	};
 })
